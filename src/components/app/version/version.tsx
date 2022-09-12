d8e6e21540d56f99c5ad22877bcac52d8ef25f73
Primož Mavsar <p.mavsar@cargox.io>
2022-09-12 19:41:36 +0200

    Add style fixes for update notification

--- src/components/app/version/version.tsx
+++ src/components/app/version/version.tsx
@@ -1,5 +1,7 @@
 import styles from './styles.module.scss';
+import button from 'styles/button.module.scss';
 import { useLatestVersion } from '../../../utils/hooks';
+import clsx from 'clsx';
 
 const RELEASE_DOWNLOAD_LINK =
   'https://github.com/cargox-holding/cxo-relay/releases';
@@ -15,9 +17,23 @@ export function Version() {
     <div className={styles.version}>
       v{currentVersion}{' '}
       {newerAvailable ? (
-        <button onClick={openDownloadPage} className={styles.updateButton}>
-          Update available
-        </button>
+        <div className={styles.updateAvailabel}>
+          <h2>Update available</h2>
+          <p>
+            New version of CXO Relay is available. Go to GitHub and download
+            latest version.
+          </p>
+          <button
+            onClick={openDownloadPage}
+            className={clsx(
+              button.button,
+              button.buttonWhite,
+              button.buttonSmall
+            )}
+          >
+            Update
+          </button>
+        </div>
       ) : null}
     </div>
   );
